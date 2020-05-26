43c0e3154b0f2774956023a39efe49f3b5ff8d97
blackstate <josemiguelmneyra@gmail.com>
2020-05-26 15:20:10 +0800

    changed book constructor to class

--- script.js
+++ script.js
@@ -28,11 +28,13 @@ window.addEventListener("load", () => {
 })
 
 // book constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function checkList() {
